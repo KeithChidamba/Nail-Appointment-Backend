5db333becfd47e3adf1c03a05e77e7b42f98a598
Keith Chidamba <kidakwashe14@gmail.com>
2025-03-18 10:31:35 -0700

    Working backend

--- Appointments Backend/obj/Debug/net8.0/Appointments Backend.AssemblyInfo.cs
+++ Appointments Backend/obj/Debug/net8.0/Appointments Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Appointments Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63867282f38421aafe6568e26940544a4587534d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57d36b954eee759c7e10314cb2d5a6aef6bebdca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Appointments Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Appointments Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
